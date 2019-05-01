31058267c6bedc94f814968caaf035915892a28c
Blake <blake.enyart@gmail.com>
2019-05-01 15:33:48 -0600

    include shoulda matchers and rebuild routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:show, :index, :create, :update]
+      namespace :merchants do
+        get '/find', to: 'search#show', as: :find_merchant
+        get '/find_all', to: 'search#index', as: :find_merchants
+      end
+      resources :merchants, only: [:show, :index]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
