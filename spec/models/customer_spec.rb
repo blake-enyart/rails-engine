cccc64517de887a528d32677f7425289eaf7ce01
Blake <blake.enyart@gmail.com>
2019-05-01 15:31:59 -0600

    Add test for relationships in each model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationship(s)' do
+    it { should have_many :invoices }
+  end
 end
