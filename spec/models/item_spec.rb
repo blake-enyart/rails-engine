56b7c145eaac26d8942f0f4ffc9ced79c38b1eeb
Blake <blake.enyart@gmail.com>
2019-05-01 15:38:44 -0600

    Pass all relationship test for models

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Item, type: :model do
   describe 'relationship(s)' do
     it { should belong_to :merchant }
     it { should have_many :invoice_items }
+    it { should have_many(:invoices).through :invoice_items }
   end
 end
