a7879b978b5b7dfcee63a5ee147c92c65dc18f6e
Blake <blake.enyart@gmail.com>
2019-05-02 10:57:03 -0600

    readjust syntax of total_revenue_date method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,6 +8,9 @@ class Invoice < ApplicationRecord
   enum status: [:unshipped, :shipped]
 
   def self.total_revenue_date(date)
-    joins(:transactions, :invoice_items).select('SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue, invoices.updated_at::timestamp::date AS date').group('date').where('invoices.updated_at::timestamp::date = ? AND transactions.result = 1', date)[0]
+    joins(:transactions, :invoice_items)
+    .select('SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue, invoices.updated_at::timestamp::date AS date')
+    .group('date')
+    .where('invoices.updated_at::timestamp::date = ? AND transactions.result = 1', date)[0]
   end
 end
