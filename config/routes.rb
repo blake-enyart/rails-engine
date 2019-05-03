85f3569452f54d360920c92a3faba84ffff7f95d
Blake <blake.enyart@gmail.com>
2019-05-02 19:51:09 -0600

    Pass spec harness for x items by revenue and x items by amount sold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      #Merchant Routes
       namespace :merchants do
         get '/find', to: 'search#show', as: :find_merchant
         get '/random', to: 'search#show', params: {random: true}
@@ -9,15 +10,19 @@ Rails.application.routes.draw do
         get '/revenue', to: 'revenue#show'
         get '/most_items', to: 'most_items#index'
       end
-
       resources :merchants, :show do
         get '/invoices', to: 'merchants/invoices#index'
         get '/items', to: 'merchants/items#index'
         get '/revenue', to: 'merchants/revenue#show'
         get '/favorite_customer', to: 'merchants/favorite_customer#show'
       end
-
       resources :merchants, only: [:index]
+
+      #Item Routes
+      namespace :items do
+        get '/most_revenue', to: 'most_revenue#index'
+        get '/most_items', to: 'most_items#index'
+      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
