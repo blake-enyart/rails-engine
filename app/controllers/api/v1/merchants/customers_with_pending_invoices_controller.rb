287a2b61e8eea01ceefc91dfa2aa1ce63a498dd6
Blake <blake.enyart@gmail.com>
2019-05-04 11:07:14 -0600

    Pass spec harness with spike for BOSS MODE

---
+++ app/controllers/api/v1/merchants/customers_with_pending_invoices_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::Merchants::CustomersWithPendingInvoicesController < ApplicationController
+
+  def index
+    render json: CustomerSerializer.new(Merchant.customers_with_pending_invoices(params[:merchant_id]))
+  end
+end
